9b5c065e3bc81b1a4f9b03a35a59585261952f8c
Sheryleva <sherylevangelene.p@gmail.com>
2020-11-12 13:00:28 -0500

    Added Login Screen`

---
+++ UserLogin/ViewController.swift
@@ -0,0 +1,37 @@
+//
+//  ViewController.swift
+//  UserLogin
+//
+//  Created by Sheryl Evangelene Pulikandala on 11/12/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var username: UITextField!
+    
+    @IBOutlet weak var password: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    @IBAction func username_entered(_ sender: UITextField) {
+        
+    }
+    
+    @IBAction func password_entered(_ sender: UITextField) {
+        
+    }
+    
+    
+
+    @IBAction func buttonClicked(_ sender: UIButton) {
+        
+    }
+    
+
+}
+
